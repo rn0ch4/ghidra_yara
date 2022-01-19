a7784eaa7466e0bee6bed265aeaab4ebd8a4373a
test <test@example.com>
2022-01-20 01:01:14 +0900

    redirected stderr to findstr check

--- test_runner.bat
+++ test_runner.bat
@@ -1,1 +1,1 @@
-"%GHIDRA_INSTALL_DIR%/support/analyzeHeadless.bat" tests ghidra_yara_test -import tests/test.exe -readOnly -noanalysis -scriptPath tests -preScript test_ghidra_yara.py %1 | findstr /R /C:"^OK$"
+"%GHIDRA_INSTALL_DIR%/support/analyzeHeadless.bat" tests ghidra_yara_test -import tests/test.exe -readOnly -noanalysis -scriptPath tests -preScript test_ghidra_yara.py %1 2>&1 | findstr /R /C:"^OK$"
